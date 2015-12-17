cde71a83986206a7c5c645e314a2a487744703bd
adarshdch <adarshdch@gmail.com>
2015-12-17 17:43:54 +0530

    Updated comments

--- client/js/config/config.js
+++ client/js/config/config.js
@@ -8,10 +8,14 @@ requirejs.config({
 		//Base path of the files to be confired in requirejs. Note here that this path is relative to index.html file
     baseUrl: 'js/vendor',
 
-    //
+    // 3rd party script alias names (Easier to type "jquery" than "libs/jquery, etc")
+    // probably a good idea to keep version numbers in the file names for updates checking
+    // These paths are checked when you write like require("alias");
     paths: {
     		//Note here that this path is relative to baseUrl i.e. js/vendor
-        app: '../app'		//Application path =	js/lib + ../app = js/app folder
+        app: '../app',		//Application path =	js/lib + ../app = js/app folder
+        alias: 'actual/file/path'	//Given path does not exist but it won't throw error since we are not trying to use this alias in code.
+
     }
 });
 
